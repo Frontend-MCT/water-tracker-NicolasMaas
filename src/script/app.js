e22940116265807ce77c66869938910424e2ea13
Nicolas Maas <nicolas.maas@student.howest.be>
2019-03-27 13:31:46 +0100

    End of lesson

--- src/script/app.js
+++ src/script/app.js
@@ -1,3 +1,23 @@
 (function() {
 	console.log('💧', 'https://www.youtube.com/watch?v=ARC1w1WWxGY');
+
+	const options = {
+		units: 'ml',
+		dailyGoal: 2000,
+		mode: 'local',
+		domRefs: {
+			percentage: 	 'js-amount',
+			timeStampHolder: 'js-timestamps',
+			addButton: 		 'js-log',
+			currentGoal: 	 'js-goal',
+			currentUnits: 	 'js-units'
+		},
+		afterUpdate: function(newPercentage) {
+			console.log("It's been updated! 👍");
+		}
+	}
+
+	document.addEventListener('DOMContentLoaded', () => {
+		new ProgressTracker(options);
+	});
 })();
